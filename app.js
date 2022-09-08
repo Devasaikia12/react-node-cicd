fbe593c9f66ce0926a41f52391a1f9f68f4320a6
Devasaikia12 <devasaikia2009@gmail.com>
2022-09-08 17:54:44 +0530

    deploy

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// express midlerware for static file
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('/names', (req, res) => {
 	res.send('Twilo');
 });
